516a4e55007d5be1318512b4470288f5e065f9c4
Uendel Junior <uendeljr1@gmail.com>
2026-02-22 20:43:37 -0300

    feat: cria entidade Order

---
+++ src/main/java/com/projeto/gestao/entity/Order.java
@@ -0,0 +1,64 @@
+package com.projeto.gestao.entity;
+
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(nullable = false,precision = 10,scale = 2)
+    private BigDecimal total;
+
+    public Order() {
+    }
+
+    public Order(LocalDateTime createdAt, OrderStatus status, BigDecimal total) {
+        this.createdAt = createdAt;
+        this.status = status;
+        this.total = total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+}
