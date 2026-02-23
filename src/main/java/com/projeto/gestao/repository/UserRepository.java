76a728c3880df85e067786229ccfa9b02de1721d
Uendel Junior <uendeljr1@gmail.com>
2026-02-22 23:13:12 -0300

    feat: cria UserRepository com busca por email

---
+++ src/main/java/com/projeto/gestao/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.projeto.gestao.repository;
+
+import com.projeto.gestao.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+}
